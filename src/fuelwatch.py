94aa687fa6c0a45855f3d3ec4f6b82d16144a53c
Charles Turner <ct6g18@soton.ac.uk>
2022-12-05 19:49:06 +0800

    Working basic web scraper

---
+++ src/fuelwatch.py
@@ -0,0 +1,51 @@
+"""
+Fuelwatch: Lets you scrape tools via their RSS feed. Basically, we're gonna 
+have to send some HTML requests using the requests library, and then play with 
+the data we get back.
+"""
+
+import requests
+import json
+import os 
+import shutil
+import feedparser
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def read_json(filename: str):
+    with open(filename) as f_in:
+        return json.load(f_in)
+
+class FuelPrice():
+    def __init__(self,brand=None,product=None,region=None,suburb=None
+                ,surrounding=True):
+        self.base_URL = "https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS"
+
+        data_path = os.path.join(ROOT_DIR,"data")
+        self.brand =   read_json(os.path.join(data_path,"brand.json"))
+        self.product = read_json(os.path.join(data_path,"product.json"))
+        self.region =  read_json(os.path.join(data_path,"region.json"))
+       
+        self.payload = {}
+    
+        if brand is not None:
+            self.payload["Brand"] = self.brand[brand]
+        if product is not None:
+            self.payload["Product"] = self.product[product]
+        if region is not None:
+            self.payload["Region"] = self.region[region]
+        if suburb is not None:
+            self.payload["Suburb"] = suburb
+
+        self.payload["Surrounding"] = "yes" if surrounding is True else "no"
+
+
+        self.response = requests.get(self.base_URL,params=self.payload
+                                    ,headers={'user-agent' : ''})
+
+        self.feed = feedparser.parse(self.response.content['entries'])
+
+
+        
+
+    
