aabb0796299e64c54b8e5ba0876f09f65ebc3879
Charles Turner <ct6g18@soton.ac.uk>
2023-01-20 17:31:17 +0800

    Working prototype

---
+++ src/basic_qt.py
@@ -0,0 +1,92 @@
+from fuelwatch import FuelPrice
+
+
+from PyQt6.QtWidgets import  QApplication, QWidget, QLabel, QComboBox, QPushButton, QCheckBox
+import sys
+ 
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 500)
+        self.setWindowTitle("Fuel Watcher")
+        interval = 60
+ 
+        self.fuel = FuelPrice()
+
+        self.brand_selector = QComboBox(self)
+        self.brand_selector.addItem("Any")
+        for brand in self.fuel.get_brands():
+            self.brand_selector.addItem(brand)
+        self.brand_selector.move(100,100+ 0* interval)
+ 
+        self.product_selector = QComboBox(self)
+        self.product_selector.addItem("Any")
+        for product in self.fuel.get_products():
+            self.product_selector.addItem(product)
+        self.product_selector.move(100,100+ 1* interval)
+
+        self.region_selector = QComboBox(self)
+        self.region_selector.addItem("Any")
+        for region in self.fuel.get_regions():
+            self.region_selector.addItem(region)
+        self.region_selector.move(100,100+ 2* interval)
+
+        self.suburb_selector = QComboBox(self)
+        self.suburb_selector.addItem("Any")
+        for suburb in self.fuel.get_suburbs():
+            self.suburb_selector.addItem(suburb)
+        self.suburb_selector.move(100,100+ 3* interval)
+
+        self.surrounding = QCheckBox(self)
+        self.surrounding.toggled.connect(self.inc_surround)
+        self.surrounding.setText("Allow Surrounding Suburbs?")
+        self.surrounding.move(100,100+ 4* interval)
+
+        self.brand_selector.width =  self.product_selector.width = self.region_selector.width = self.suburb_selector.width
+
+        button = QPushButton("Submit", self)
+        button.clicked.connect(self.submit)
+
+        button.move(100,100 + 5 * interval)
+ 
+    def submit(self):
+        # Need to set defaults properly
+        region_val = self.fuel.region[self.region_selector.currentText()]
+        self.remove_any()
+        self.fuel.set_region(region=region_val)
+
+        product_val = self.fuel.product[self.product_selector.currentText()]
+        self.remove_any()
+        self.fuel.set_product(product=product_val)
+
+        suburb_val = self.suburb_selector.currentText()
+        self.remove_any()
+
+        self.fuel.set_suburb(suburb=suburb_val)
+        brand_val = self.fuel.brand[self.brand_selector.currentText()]
+        self.remove_any()
+
+        self.fuel.set_brand(brand=brand_val)    
+        self.remove_any()
+        self.fuel.set_surrounding(surrounding=self.inc_surround())
+
+        print([(key,val) for key,val in self.fuel.payload.items()])
+        self.fuel.request()
+        self.fuel.print_all()
+        
+    def remove_any(self):
+        """ Remove any items from self.fuel where self.fuel.key == "Any" """
+        new_dict = self.fuel.payload.copy()
+        for key, val in new_dict.items():
+            if val == "Any":
+                self.fuel.payload.pop(key)
+
+    def inc_surround(self):
+        print("Selected: ", self.surrounding.isChecked())
+        return self.surrounding.isChecked()
+ 
+ 
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
--- src/fuelwatch.py
+++ src/fuelwatch.py
@@ -23,9 +23,10 @@ class FuelPrice():
         self.base_URL = "https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS"
 
         data_path = os.path.join(ROOT_DIR,"data")
-        self.brand =   read_json(os.path.join(data_path,"brand.json"))
+        self.brand   = read_json(os.path.join(data_path,"brand.json"))
         self.product = read_json(os.path.join(data_path,"product.json"))
-        self.region =  read_json(os.path.join(data_path,"region.json"))
+        self.region  = read_json(os.path.join(data_path,"region.json"))
+        self.suburb  = read_json(os.path.join(data_path,"suburb.json"))
        
         self.payload = {}
     
@@ -36,6 +37,7 @@ class FuelPrice():
             self.fuel_name = product
         else:
             self.fuel_name = "Unleaded Petrol"
+        
         if region is not None:
             self.payload["Region"] = self.region[region]
         if suburb is not None:
@@ -43,19 +45,43 @@ class FuelPrice():
 
         self.payload["Surrounding"] = "yes" if surrounding is True else "no"
 
+        self.request()
 
+    
+    def request(self):
         self.response = requests.get(self.base_URL,params=self.payload
                                     ,headers={'user-agent' : ''})
-
         self.feed = feedparser.parse(self.response.content)['entries']
-
         self.data_fields = self.feed[0].keys()
+
 
     def print_all(self):
         for feed_val in self.feed:
             print(f"{feed_val['brand']} sells {self.fuel_name} for {feed_val['price']} at {feed_val['address']}")
     
-
-        
-
+    def get_brands(self):
+        return [key for key in self.brand.keys()]
     
+    def get_suburbs(self):
+        return [key for key in self.suburb.keys()]
+
+    def get_regions(self):
+        return [key for key in self.region.keys()]
+
+    def get_products(self):
+        return [key for key in self.product.keys()]
+
+    def set_brand(self,brand):
+        self.payload["Brand"] = brand
+    
+    def set_suburb(self,suburb):
+        self.payload["Suburb"] = suburb
+
+    def set_region(self,region):
+        self.payload["Region"] = region
+
+    def set_product(self,product):
+        self.payload["Product"] = product
+
+    def set_surrounding(self,surrounding):
+        self.payload["Surrounding"] = "yes" if surrounding is True else "no"
